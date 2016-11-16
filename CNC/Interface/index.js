75bcba5d7f77f554a7f8c0749a07aec442aaf3a0
freakdran <dominik.holweck@web.de>
2016-11-16 17:15:31 +0100

    fixed 1.1

--- CNC/Interface/index.js
+++ CNC/Interface/index.js
@@ -6,28 +6,10 @@ var _navigate = function(id) {
     element.className = 'active';
   }
 
-  //  var others = document.querySelector('section');
-
   var others = [].slice.call(document.querySelectorAll('section'));
   for(var i = 0; i < others.length; i++) {
     if(others[i] !== element) {
       others[i].className = '';
     }
   }
-
-  //var otherso = others.map(  )
-
-
-
-
-
-
-
-
-  //console.log(otherso);
-
-
-  // TODO: query all other elements into Array
-  // TODO: set className of others to ''
-
 };
