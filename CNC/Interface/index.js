30fcbed6aa91a45bf5f88de76612dd35e28eafb7
Emmeris <jonas.lagaude@yahoo.de>
2017-01-18 13:45:19 +0100

    fixed eslint in interface

--- CNC/Interface/index.js
+++ CNC/Interface/index.js
@@ -1,22 +1,22 @@
 
-var _navigate = function(id) {
+let _navigate = function(id) {
 
-  var element = document.querySelector('#' + id);
-  if (element !== null) {
-    element.className = 'active';
-  }
-  var menu = document.querySelector('#inputSelector');
+	let element = document.querySelector('#' + id);
+	if (element !== null) {
+		element.className = 'active';
+	}
+	let menu = document.querySelector('#inputSelector');
 
-  if(element.id === 'tasks') {
-    menu.className = 'active';
-  } else {
-    menu.className = '';
-  }
+	if (element.id === 'tasks') {
+		menu.className = 'active';
+	} else {
+		menu.className = '';
+	}
 
-  var others = [].slice.call(document.querySelectorAll('section'));
-  for(var i = 0; i < others.length; i++) {
-    if(others[i] !== element) {
-      others[i].className = '';
-    }
-  }
+	let others = [].slice.call(document.querySelectorAll('section'));
+	for (let i = 0; i < others.length; i++) {
+		if (others[i] !== element) {
+			others[i].className = '';
+		}
+	}
 };
